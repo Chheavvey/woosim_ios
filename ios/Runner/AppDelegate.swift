33f654e8225ea01f3b2c3f0f3edd70d3f771a31d
Chheavvey <chheavvey.pann@gmail.com>
2026-06-01 09:35:26 +0700

    ios_build_unsign

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,12 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  private var printerChannelHandler: IOSPrinterChannelHandler?
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller = window?.rootViewController as! FlutterViewController
-    printerChannelHandler = IOSPrinterChannelHandler(messenger: controller.binaryMessenger)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
